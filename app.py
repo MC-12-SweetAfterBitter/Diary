bbf0cb37595c145b8edb63463bba5c2936804d5a
MZ16 <acws12@nate.com>
2022-03-28 03:42:27 +0900

    로그인 세션 추가중

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, jsonify, request
-
+from flask import Flask, render_template, jsonify, request, session, redirect, url_for, escape
+from datetime import timedelta
 app = Flask(__name__)
 
 from pymongo import MongoClient
@@ -7,16 +7,25 @@ from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 db = client.dbhomework
 
+app.secret_key = 'secretkey'  # secret_key는 서버상에 동작하는 어플리케이션 구분하기 위해 사용하고 복잡하게 만들어야 합니다.
+app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(minutes=1) # 로그인 지속시간을 정합니다. 현재 1분
 
-## HTML 화면 보여주기
+
+# 회원가입 화면
 @app.route('/')
-def homework():
+def register_page():
     return render_template('register.html')
+# 로그인 화면
+@app.route('/login')
+def login_page():
+    if "name" in session:
+        return jsonify({"ans":"success"},{"msg" : "환영합니다 {}님".format(escape(session['name']))})
+    return render_template('login.html')
 
 
 # 회원가입(POST) API
 @app.route('/register', methods=['POST'])
-def user_infor():
+def resgister():
     name_receive = request.form['name_give']
     email_receive = request.form['email_give']
     password_receive = request.form['password_give']
@@ -27,8 +36,12 @@ def user_infor():
 
 
     for list in res:
+        # 공백 처리, 해당 부분에서 약간의 오류를 발생시키면 html 스크립트 공백체크가 작동한다..
+        if list['nae'] == " " or list['emil'] == " " or list['pasword'] == " " or list['pcf'] == " ":
+            print(list['name'], list['email'], list['pssword'], list['pwcf'])
+            return jsonify({'ans': 'fail', 'msg': '입력되지 않은 값이 있습니다'})
         # 회원가입 시 중복 ID, Email 처리
-        if list['name'] == name_receive or list['email'] == email_receive:
+        elif list['name'] == name_receive or list['email'] == email_receive:
             return jsonify({'ans': 'fail', 'msg': '아이디 또는 이메일 중복!'})
         # 2차 비밀번호 체크
         elif pwcf_receive != password_receive:
@@ -44,11 +57,31 @@ def user_infor():
     return jsonify({'ans':'success', 'msg': '회원가입 완료'})
 
 
-# 주문 목록보기(Read) API
-@app.route('/order', methods=['GET'])
-def view_orders():
-    orders = list(db.diary.find({},{'_id':False})) #조건
-    return jsonify({'all_orders': orders})
+# 로그인(POST) API
+@app.route('/login/res', methods=['POST',"GET"])
+def login():
+    name_receive = request.form['name_give']
+    password_receive = request.form['password_give']
+    print(name_receive, password_receive)
+    res = db.diary.find({}, {'_id': False})
+
+    for list in res:
+        # DB의 id와 비밀번호 확인
+        if list['name'] == name_receive and list['password'] == password_receive:
+            #세션 할당 후
+            session['name'] = request.form['name_give']
+            #
+            return render_template(url_for(login_page))
+    return render_template(url_for(login_page))
+
+
+
+
+# # 주문 목록보기(Read) API
+# @app.route('/order', methods=['GET'])
+# def view_orders():
+#     orders = list(db.diary.find({},{'_id':False})) #조건
+#     return jsonify({'all_orders': orders})
 
 
 if __name__ == '__main__':
