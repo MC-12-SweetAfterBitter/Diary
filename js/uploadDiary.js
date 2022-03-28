0c865730e9c0f6786436b2e10c2f7b549d1f38b8
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-03-28 20:40:41 +0900

    added writing diary
    날짜, 일기장 내용도 포함되어 list 추가되게 수정

--- js/uploadDiary.js
+++ js/uploadDiary.js
@@ -1,35 +1,31 @@
-const uploadButton = document.querySelector(
-  ".upload_button .btn .btn-outline-secondary"
-); // 업로드 버튼 가져오기
+const uploadButton = document.querySelector(".upload_button"); // 업로드 버튼 가져오기
 const diaryTitle = document.querySelector(".title input"); // 일기장 제목
+const Year = document.querySelector("#year"); // 연도
+const Month = document.querySelector("#month"); // 월
+const Dates = document.querySelector("#date"); // 일
+const diaryContent = document.querySelector("#floatingTextarea2"); // 일기장 내용
 
 function uploadDiary(event) {
-  let title = diaryTitle.value;
+  event.preventDefault();
+  let title = diaryTitle.value; // 일기장 제목
+  let diaryYear = Year.options[Year.selectedIndex].value; // 작성 연도
+  let diaryMonth = Month.options[Month.selectedIndex].value; // 작성 월
+  let diaryDate = Dates.options[Dates.selectedIndex].value; // 작성 일
+  let content = diaryContent.value; // 일기장 내용
 
   let temp_html = `<div class="card">
-  <div class="card-header">
-    <h5>${title}</h5>
-    <h5>2022-03-25</h5>
-  </div>
-  <div class="card-body">
-    <p class="card-text">
-      오늘은 ~~ 했다. ~ 를 먹었고, ~와 함께 ~에 놀러갔다! 재밌었다.
-    </p>
-  </div>
-</div>`;
+                    <div class="card-header">
+                      <h5>${title}</h5>
+                      <h5>${diaryYear}-${diaryMonth}-${diaryDate}</h5>
+                    </div>
+                    <div class="card-body">
+                      <p class="card-text">
+                        ${content}
+                      </p>
+                    </div>
+                  </div>`;
 
   $(".diary_lists").append(temp_html);
 }
 
 uploadButton.addEventListener("click", uploadDiary);
-/*
-  const diaryInput = document.querySelector(".diary_content");
-*/
-
-/*
-  event.preventDefault();
-  const uploadedDiary = diaryInput.value;
-  const uploadedTitle = diaryTitle.value;
-  diaryInput.value = "";
-  console.log(`${uploadedDiary}`);
-  */
