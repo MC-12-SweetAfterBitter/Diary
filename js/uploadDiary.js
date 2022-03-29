a815738a1f6406a2806e353458910d82cd652f58
boeunnnnn <102156439+boeunnnnn@users.noreply.github.com>
2022-03-30 00:14:08 +0900

    3/30 보은 회의 후 수정사항(좋아요는 진행중)

--- js/uploadDiary.js
+++ js/uploadDiary.js
@@ -1,22 +1,15 @@
 const uploadButton = document.querySelector(".upload_button"); // 업로드 버튼 가져오기
 const diaryTitle = document.querySelector(".title input"); // 일기장 제목
-const Year = document.querySelector("#year"); // 연도
-const Month = document.querySelector("#month"); // 월
-const Dates = document.querySelector("#date"); // 일
 const diaryContent = document.querySelector("#floatingTextarea2"); // 일기장 내용
 
 function uploadDiary(event) {
   event.preventDefault();
   let title = diaryTitle.value; // 일기장 제목
-  let diaryYear = Year.options[Year.selectedIndex].value; // 작성 연도
-  let diaryMonth = Month.options[Month.selectedIndex].value; // 작성 월
-  let diaryDate = Dates.options[Dates.selectedIndex].value; // 작성 일
   let content = diaryContent.value; // 일기장 내용
 
   let temp_html = `<div class="card">
                     <div class="card-header">
                       <h5>${title}</h5>
-                      <h5>${diaryYear}-${diaryMonth}-${diaryDate}</h5>
                     </div>
                     <div class="card-body">
                       <p class="card-text">
