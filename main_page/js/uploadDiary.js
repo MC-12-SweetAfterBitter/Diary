937850e4864a6d7f6e7386e1f0bd17367e3ad186
DHL68 <ldh1255@naver.com>
2022-04-01 21:01:22 +0900

    22. 4. 1. diary_front 개인/공감 page ver.05

--- main_page/js/uploadDiary.js
+++ main_page/js/uploadDiary.js
@@ -19,16 +19,17 @@ function uploadDiary_gonggam(event_1) {
 
   let temp_html = `<div class="card">
                     <div class="card-header">
-                      <h5 class="card-header-title">작성자 : ${name} / 제목 : ${title}</h5>
-                        <!--공감 버튼-->
-                        <button type="button" class="btn btn-outline-danger">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart"
-                                 viewBox="0 0 16 16">
-                                <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"></path>
-                            </svg>
-                        </button>
+                      <h5 class="card-header-title">제목 : ${title} <br> <label style="font-size: 16px"> 작성자 : ${name} </label></h5>
+                      
+                      <!--공감 버튼-->
+                      <button type="button" class="btn btn-outline-danger">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart"
+                               viewBox="0 0 16 16">
+                              <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"></path>
+                          </svg>
+                      </button>
                     </div>
-                    <div class="card-body">
+                    <div class="card-body au">
                       <p class="card-text">
                         ${content}
                       </p>
@@ -55,7 +56,7 @@ function uploadDiary_personal(event_2) {
                       <h5 class="card-header-title">제목 : ${title}</h5>
                       <h5 class="card-header-title">날짜 : ${diaryYear}-${diaryMonth}-${diaryDate}</h5>
                     </div>
-                    <div class="card-body">
+                    <div class="card-body au">
                       <p class="card-text">
                         ${content}
                       </p>
